cb4535cd1c821b05444697a7ba0de45e37104d14
Kiran S Baliga <60563196+kiransbaliga@users.noreply.github.com>
2020-10-17 21:59:49 +0530

    Initial works on UI

---
+++ FLUTTER/moosik/lib/mp3player/MusicPlayerScreen.dart
@@ -0,0 +1,23 @@
+/*import 'package:flutter/material.dart';
+
+class MusicPlayerScreen extends StatefulWidget {
+  @override
+  _MusicPlayerScreenState createState() => _MusicPlayerScreenState();
+}
+
+class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("MOOSIK"),
+      ),
+      body:Container(
+        padding: EdgeInsets.all(20.0),
+      ),
+    );
+  }
+}
+
+
+*/
